31e628f620e6e97f085cd4bfd9981d2f34c488c4
abenstirling <abenstirling@pm.me>
2023-07-15 11:41:16 -0700

    Adding framework for presentation and app

---
+++ qualcomm.md
@@ -0,0 +1,6 @@
+#  Prize qualifications
+- mobile first and novel use cae
+- qualcomm nerural processing sdk
+- runs on the hardware accelerator
+
+# Acutal Talk 
---
+++ sdxai/sdxai/sdxaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sdxaiApp.swift
+//  sdxai
+//
+//  Created by Ben Stirling on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct sdxaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
