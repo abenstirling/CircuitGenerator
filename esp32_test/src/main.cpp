2a73566cb5afeabdb306385598e38a31ff3cda25
abenstirling <abenstirling@pm.me>
2023-07-15 12:03:51 -0700

    Adding framework and working code for esp32!

---
+++ esp32_test/src/main.cpp
@@ -0,0 +1,20 @@
+// Include the required libraries
+#include <Arduino.h>
+
+// Define the pin number where the LED is connected
+const int ledPin = 2;
+
+void setup() {
+  // Set the LED pin as an output
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // Turn the LED on
+  digitalWrite(ledPin, HIGH);
+  delay(1000); // Wait for 1 second
+  
+  // Turn the LED off
+  digitalWrite(ledPin, LOW);
+  delay(1000); // Wait for 1 second
+}
